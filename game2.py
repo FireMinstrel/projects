3598a0fd8303c6be88bbee1c1d9003606a0bf293
FireMinstrel <44679617+FireMinstrel@users.noreply.github.com>
2018-12-05 19:05:16 -0500

    Update game2.py

--- game2.py
+++ game2.py
@@ -1,8 +1,45 @@
 import random
+import sys
 
 CELLS = {(0, 0):"bedroom", (0, 1):"bathroom", (0, 2):"master bedroom",
          (1, 0):"west hallway", (1, 1):"center hallway", (1, 2):"east hallway",
          (2, 0):"living room", (2, 1):"entryway", (2, 2):"kitchen"}
+
+def title_screen_selections():
+    option = input("> ")
+    if option.lower() == ("play"):
+        print("You heard rumors about an abandoned, supposedly haunted house.\nThey say there's treasure inside somewhere.\  Guess who's going adventuring...?")
+        main()
+    elif option.lower() == ("help"):
+        help_menu()
+    elif option.lower() == ("quit"):
+        sys.exit()
+    while option.lower() not in ['play', 'help', 'quit']:
+        print("Please enter a valid command.")
+        option = input("> ")
+        if option.lower() == ("play"):
+            main()
+        elif option.lower() == ("help"):
+            help_menu()
+        elif option.lower() == ("quit"):
+            sys.exit()
+
+def title_screen():
+    print('######################')
+    print('###### Welcome! ######')
+    print('######################')
+    print('        -Play-        ')
+    print('        -Help-        ')
+    print('        -Quit-        ')
+    title_screen_selections()
+
+def help_menu():
+    print('######################')
+    print('###### Welcome! ######')
+    print('######################')
+    print('Use \'n\', \'s\', \'e\', \'w\' to move.')
+    print('Good luck and have fun!')
+    title_screen()
 
 def get_locations():
   start = CELLS[(2,1)]
@@ -56,26 +93,44 @@ def draw_map(player):
         print(tile.format('_|'))
 
 player = get_locations()
-print("Welcome to the dungeon!")
-player = (2, 1)
-while True:
-  moves = get_moves(player)
-  
-  print("You're currently in the {}".format(CELLS[player]))
-  
-  draw_map(player)
-  
-  print("You can move {}".format(moves))
-  print("Enter QUIT to quit")
-  
-  move = input("> ")
-  move = move.upper()
-  
-  if move == 'QUIT':
-    break
-    
-  if move in moves:
-    player = move_player(player, move)
-  else:
-    print("** Walls are hard, stop walking into them! **")
-    continue
+
+
+def main():
+    print("Welcome to the dungeon!")
+    treasure_found = False
+    player = (2, 1)
+    while True:
+      moves = get_moves(player)
+      
+      print("You're currently in the {}".format(CELLS[player]))
+      
+      draw_map(player)
+      
+      print("You can move {}".format(moves))
+      print("Enter QUIT to quit")
+      
+      move = input("> ")
+      move = move.upper()
+      
+      if move == 'QUIT':
+        break
+        
+      if move in moves:
+        player = move_player(player, move)
+      else:
+        print("** CRASH! **")
+        continue
+      if treasure_found and player == (2, 1):
+        print("Congratulations! You have solved the puzzles and found the treasure!\nNow go sell it on eBay!"
+                  + "Also, that house wasn't haunted...bummer.  "
+                  + "\nTHE END")
+        sys.exit()
+      elif not treasure_found and player == (0, 2):
+        treasure_found = True
+        print("You see a box in the corner. You walk over to it and open it.  TREASURE!"
+                  + "You grab the box.  The treasure is now yours!")
+      else:
+        continue
+
+title_screen()
+main()  
